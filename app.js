7a34cb1190566bfb9bed11600a4602ac6bf95ce3
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2020-04-23 23:42:31 +0200

    modified:   app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const OAuth2Data = require('./gkeys.json')
 
 const app = express()
 
-
 const CLIENT_ID = OAuth2Data.web.client_id; 
 const CLIENT_SECRET = OAuth2Data.web.client_secret;
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
