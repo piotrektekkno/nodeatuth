85d1a3f5a694523d55ad3ffad991c3b35984836e
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2020-04-23 23:47:12 +0200

    modified:   app.js 	copy.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const { google } = require('googleapis');
 const express = require('express')
 const OAuth2Data = require('./gkeys.json')
@@ -60,3 +61,25 @@ app.get('/auth/google/callback', function (req, res) {
 
 const port = process.env.port || 5000
 app.listen(port, () => console.log(`Server running at ${port}`));
+*/
+
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  var strP = '<br>';
+  var tmpStr =''
+  var q = req.query;
+  for (var a in q){
+    tmpStr = 'Parametr w url: ' + a + ' wartość: ' + q[a];
+    console.log(tmpStr);
+    strP += '<br>' + tmpStr
+  }
+  res.send('Hello World! on Heroku platform :) ' +
+  '<br>Deploy from Github details: <b>piotrektekkno/herokuNodeJS </b>' +
+  strP);
+
+
+});
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
