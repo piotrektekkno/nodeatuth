1a019944dc4c61db6e0985aedfe9edddbfe4c60a
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2020-04-23 20:54:59 +0200

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
@@ -9,3 +10,68 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+*/
+
+const { google } = require('googleapis');
+const express = require('express')
+const OAuth2Data = require('./gkeys.json')
+
+const app = express()
+
+const CLIENT_ID = OAuth2Data.web.client_id; 
+const CLIENT_SECRET = OAuth2Data.web.client_secret;
+const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+var authed = false;
+
+app.get('/', (req, res) => {
+    if (!authed) {
+        // Generate an OAuth URL and redirect there
+        const url = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: 'https://www.googleapis.com/auth/userinfo.profile'
+        });
+        console.log(url)
+        res.redirect(url);
+    } else {
+        const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+        gmail.users.labels.list({
+            userId: 'me',
+        }, (err, res) => {
+            if (err) return console.log('The API returned an error: ' + err);
+            const labels = res.data.labels;
+            if (labels.length) {
+                console.log('Labels:');
+                labels.forEach((label) => {
+                    console.log(`- ${label.name}`);
+                });
+            } else {
+                console.log('No labels found.');
+            }
+        });
+        res.send('Logged in')
+    }
+})
+
+app.get('/auth/google/callback', function (req, res) {
+    const code = req.query.code
+    if (code) {
+        // Get an access token based on our OAuth code
+        oAuth2Client.getToken(code, function (err, tokens) {
+            if (err) {
+                console.log('Error authenticating')
+                console.log(err);
+            } else {
+                console.log('Successfully authenticated');
+                oAuth2Client.setCredentials(tokens);
+                authed = true;
+                res.redirect('/')
+            }
+        });
+    }
+});
+
+const port = process.env.port || 5000
+app.listen(port, () => console.log(`Server running at ${port}`));
