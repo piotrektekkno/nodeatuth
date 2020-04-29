c1f9cdfed19a96905b80802fe517de953038ef31
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2020-04-29 21:36:03 +0200

    commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const { google } = require('googleapis');
 const express = require('express')
 const request = require('request')
 const OAuth2Data = require('./gkeys.json')
-const { Client } = require('pg');
 
 const app = express();
 
@@ -10,7 +9,6 @@ const app = express();
 const CLIENT_ID = OAuth2Data.web.client_id; 
 const CLIENT_SECRET = OAuth2Data.web.client_secret;
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
-client.connect();
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 var appToken = '';
