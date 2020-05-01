1729b681e97ff58d96887543cd13f5ef7cdbe01e
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2020-05-01 20:41:39 +0200

    commit

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const CLIENT_ID = OAuth2Data.web.client_id;
 const CLIENT_SECRET = OAuth2Data.web.client_secret;
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
 
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  });
+
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 var appToken = '';
 var authed = false;
