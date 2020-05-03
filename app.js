e14091be48a6a10158c08dc5c1c32fa2d2092ed3
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2020-05-03 12:16:04 +0200

    commit

--- app.js
+++ app.js
@@ -71,20 +71,22 @@ app.get('/', (req, res) => {
               console.log(JSON.stringify(row));
               userExists = parseInt(JSON.parse(JSON.stringify(row)).isuser,10);
             }
+
+            if(userExists){
+                console.log(loggedUser + ' został stworzony już isnieje dodanie 1 do kolumny Count' );
+                client.query('UPDATE public."Users" SET Counter = Counter + 1 WHERE Name = \'' + loggedUser + '\'' , (err, r) => {
+                    if (err) throw err;
+                });
+            } else {
+                console.log(loggedUser + ' nie istnieje dodanie użytkowanika' );
+                client.query('INSERT INTO public."Users" (NAME) VALUES  (\'' + loggedUser + '\')' , (err, r) => {
+                    if (err) throw err;
+                });
+            }
         
         });
 
-        if(userExists){
-            console.log(loggedUser + ' został stworzony już isnieje dodanie 1 do kolumny Count' );
-            client.query('UPDATE public."Users" SET Counter = Counter + 1 WHERE Name = \'' + loggedUser + '\'' , (err, r) => {
-                if (err) throw err;
-            });
-        } else {
-            console.log(loggedUser + ' nie istnieje dodanie użytkowanika' );
-            client.query('INSERT INTO public."Users" (NAME) VALUES  (\'' + loggedUser + '\')' , (err, r) => {
-                if (err) throw err;
-            });
-        }
+  
 
             
         client.query('SELECT * FROM public."Users"', (err, r) => {
